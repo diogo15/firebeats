da0327abab9d1b279da377e82ac01e745b936118
FABIAN <FABIAN@ELDRITCH>
2022-10-15 18:29:35 -0600

    Database context created, database implemented. Change appsettings connection string for local usage

---
+++ FireBeats/Domain/Canciones.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FireBeats.Domain
+{
+    public class Canciones
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(70)]
+        public string CancionName { get; set; }
+        public string CancionPath { get; set; }
+  
+    }
+}
