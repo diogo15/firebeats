d0ba49c6a153740bec82f7f5c88af050d03d8951
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-11-07 21:42:09 -0600

    New project api

---
+++ FireBeats.API/Domain/Cities.cs
@@ -0,0 +1,15 @@
+﻿namespace FireBeats.Domain
+{
+    public class Cities
+    {
+        public Guid Id { get; set; }
+        public string CityName { get; set; }
+
+        /*
+         * "Foreign Keys"
+         * Relation 1:M to the Countries Class
+         * The class you want to be the 1 in the relationship
+         */
+        public virtual Countries Countries { get; set; }
+    }
+}
---
+++ FireBeats.API/Domain/Users.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FireBeats.Domain
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("User")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string UserPassword { get; set; }
+
+        public bool Artist { get; set; }
+
+        // Foreign Keys ->
+        // Reference to the class wanted to be the related to
+        public Cities Cities { get; set; }
+
+        // One User can have Many Playlists
+        public ICollection<Playlists> Playlists { get; set; }
+    }
+}
