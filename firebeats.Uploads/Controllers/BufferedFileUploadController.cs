39cd095dc5f21b4179efaf6a8ee624c64c75f278
Diogo15 <luisdiegore@hotmail.com>
2022-11-10 01:35:34 -0600

    Uploads in the server added, Cors policy for localhost:4200 added

---
+++ firebeats.Uploads/Controllers/BufferedFileUploadController.cs
@@ -0,0 +1,51 @@
+﻿using Firebeats.Uploads.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firebeats.Uploads.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BufferedFileUploadController : ControllerBase
+    {
+        private readonly IBufferedFileUploadService _bufferedFileUploadService;
+        private readonly IWebHostEnvironment _environment;
+
+        public BufferedFileUploadController(IBufferedFileUploadService bufferedFileUploadService, IWebHostEnvironment environment)
+        {
+            _bufferedFileUploadService = bufferedFileUploadService;
+            this._environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(IFormFile file)
+        {
+            try
+            {
+                if (await _bufferedFileUploadService.UploadFile(file))
+                {
+                    var path = getFilePath(file.FileName);
+
+                    return StatusCode(StatusCodes.Status200OK, path);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status200OK, "File Upload Failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log ex
+                return StatusCode(StatusCodes.Status200OK, ex);
+            }
+
+        }
+
+
+        [NonAction]
+        public string getFilePath(string songName)
+        {
+            return this._environment.WebRootPath + "\\UploadedFiles\\" + songName;
+        }
+    }
+}
---
+++ firebeats.Uploads/Interfaces/IBufferedFileUploadService.cs
@@ -0,0 +1,7 @@
+﻿namespace Firebeats.Uploads.Interfaces
+{
+    public interface IBufferedFileUploadService
+    {
+        Task<bool> UploadFile(IFormFile file);
+    }
+}
