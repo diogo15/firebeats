1bd66a509a1de106d59b8a7839f7ad1e260c2db6
Diogo15 <luisdiegore@hotmail.com>
2022-11-10 13:50:15 -0600

    DTO extension missing.

--- FireBeats.API/Extensions/Extensions.cs
+++ FireBeats.API/Extensions/Extensions.cs
@@ -5,10 +5,12 @@ namespace FireBeats.API.Extensions
 {
     public static class Extensions
     {
+        /*
         public static UserDTO AsDTO(this Users user)
         {
             return new UserDTO(user.Id, user.UserName, user.UserEmail, user.UserPassword, user.Artist, user.Cities.CityName);
         }
+        */
 
         public static CountryDTO AsDTO(this Countries country)
         {
