e39ea395df702691f0d95c037c5f00c06741c77e
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-11-17 18:50:27 -0600

    genres api blablabla

---
+++ FireBeats.API/Controllers/GenresController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FireBeats.API.Domain;
+using FireBeats.Context;
+using FireBeats.API.DTOs;
+
+namespace FireBeats.API.Controllers
+{
+    [Route("api/genre")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly FireBeatsContext _context;
+
+        public GenresController(FireBeatsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genres>>> GetAsync()
+        {
+            return await _context.Genres.ToListAsync();
+        }
+
+        // GET: api/Genres/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genres>> GetByIdAsync(Guid id)
+        {
+            var genres = await _context.Genres.FindAsync(id);
+
+            if (genres == null)
+            {
+                return NotFound();
+            }
+
+            return genres;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Genres>> PostAsync(GenreCreatedDTO postedGenre)
+        {
+            var newGenre = new Genres
+            { 
+                Id = Guid.NewGuid(),
+                GenreName = postedGenre.GenreName,
+            };
+
+            _context.Genres.Add(newGenre);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        // PUT: api/Genres/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenres(Guid id, GenreUpdatedDTO genreUpdated)
+        {
+            var getGenre = await _context.Genres.FindAsync(id);
+            if (getGenre == null)
+                return StatusCode(StatusCodes.Status404NotFound, "No data found");
+
+            getGenre.GenreName = genreUpdated.GenreName;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenresExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var genres = await _context.Genres.FindAsync(id);
+            if (genres == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genres.Remove(genres);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GenresExists(Guid id)
+        {
+            return _context.Genres.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FireBeats.API/Domain/Genres.cs
@@ -0,0 +1,13 @@
+﻿using FireBeats.Domain;
+using System.Text.Json.Serialization;
+
+namespace FireBeats.API.Domain
+{
+    public class Genres
+    {
+        public Guid Id { get; set; }
+        public string GenreName { get; set; } = string.Empty;
+        [JsonIgnore]
+        public virtual ICollection<Songs> Songs { get; set; }
+    }
+}
